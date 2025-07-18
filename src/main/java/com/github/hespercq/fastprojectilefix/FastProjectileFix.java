b18c4766a2de5922ef4c52b5f893c5aafa771d2c
HesperCQ <HesperCQ@gmail.com>
2025-07-18 19:44:20 +0200

    1.0.0 - Cleanup Docs

--- src/main/java/com/github/hespercq/fastprojectilefix/FastProjectileFix.java
+++ src/main/java/com/github/hespercq/fastprojectilefix/FastProjectileFix.java
@@ -10,16 +10,11 @@ import org.slf4j.Logger;
 @Mod(FastProjectileFix.MODID)
 public class FastProjectileFix
 {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "fastprojectilefix";
-    // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
 
     public FastProjectileFix(FMLJavaModLoadingContext context)
     {
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-		LOGGER.info("Hello from fastprojectilefix :)");
     }
 }
