035052fa1130a15552374f4dd635b0185ece258f
HesperCQ <hespercq@gmail.com>
2026-04-06 00:22:44 +0200

    1.0.1 - Only unclamp, no short to int

--- src/main/java/com/github/hespercq/fastprojectilefix/mixin/MixinClientboundSetEntityMotionPacket.java
+++ src/main/java/com/github/hespercq/fastprojectilefix/mixin/MixinClientboundSetEntityMotionPacket.java
@@ -6,8 +6,10 @@ import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import io.netty.buffer.ByteBuf;
 import net.minecraft.world.phys.Vec3;
 
 import net.minecraft.network.FriendlyByteBuf;
@@ -34,7 +36,7 @@ public class MixinClientboundSetEntityMotionPacket {
 	@Final
 	@Mutable
 	private int za;
-	
+
 	// Constructor from Vector
 	@Inject(method = "<init>(ILnet/minecraft/world/phys/Vec3;)V", at = @At(value = "RETURN"), cancellable = true)
 	public void ClientboundSetEntityMotionPacket(int entityId, Vec3 motionVector, CallbackInfo ci) {
@@ -44,22 +46,24 @@ public class MixinClientboundSetEntityMotionPacket {
 		this.za = (int) (motionVector.z * 8000.0D);
 	}
 
-	// Constructor from Packet
+	/*
+
+	// Constructor from Packet => Redirect buf.readShort -> buf.readInt
 	@Inject(method = "<init>(Lnet/minecraft/network/FriendlyByteBuf;)V", at = @At(value = "RETURN"), cancellable = true)
-	public void ClientboundSetEntityMotionPacket(FriendlyByteBuf buf, CallbackInfo ci) {
-        this.id = buf.readVarInt();
-        this.xa = buf.readInt();
-        this.ya = buf.readInt();
-        this.za = buf.readInt();
+	public void redirect_constructor_FriendlyByteBuf_readShort(FriendlyByteBuf buf, CallbackInfo ci) {
+		this.id = buf.readVarInt();
+		this.xa = buf.readInt();
+		this.ya = buf.readInt();
+		this.za = buf.readInt();
 	}
 
-	// Write Packet
-	@Inject(method = "write", at = @At(value = "HEAD"), cancellable = true)
-	public void readPacketData(FriendlyByteBuf buf, CallbackInfo ci) {
-		ci.cancel();
-		buf.writeVarInt(this.id);
-		buf.writeInt(this.xa);
-		buf.writeInt(this.ya);
-		buf.writeInt(this.za);
+	// write => Redirect buf.writeShort -> buf.writeInt
+	@Redirect(
+		method = "write",
+		at = @At(value = "INVOKE", target = "Lio/netty/buffer/ByteBuf;writeShort(I)Lnet/minecraft/network/FriendlyByteBuf;"))
+	private ByteBuf redirect_write_writeShort(FriendlyByteBuf buf, int value) {
+		return buf.writeInt(value);
 	}
+
+	 */
 }
