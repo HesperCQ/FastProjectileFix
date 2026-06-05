06a32a48ded0845c273ec1aec107ae5e9c26b6d6
HesperCQ <HesperCQ@gmail.com>
2026-06-05 20:29:03 +0200

    [WIP] Init Commit

--- src/main/java/com/github/hespercq/fastprojectilefix/FastProjectileFix.java
+++ src/main/java/com/github/hespercq/fastprojectilefix/FastProjectileFix.java
@@ -1,20 +1,7 @@
 package com.github.hespercq.fastprojectilefix;
 
-import com.mojang.logging.LogUtils;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
+import net.neoforged.fml.common.Mod;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod(FastProjectileFix.MODID)
-public class FastProjectileFix
-{
-    public static final String MODID = "fastprojectilefix";
-    public static final Logger LOGGER = LogUtils.getLogger();
-
-    public FastProjectileFix(FMLJavaModLoadingContext context)
-    {
-        MinecraftForge.EVENT_BUS.register(this);
-    }
+@Mod("fastprojectilefix")
+public class FastProjectileFix {
 }
--- src/main/java/com/github/hespercq/fastprojectilefix/mixin/MixinClientboundSetEntityMotionPacket.java
+++ src/main/java/com/github/hespercq/fastprojectilefix/mixin/MixinClientboundSetEntityMotionPacket.java
@@ -2,12 +2,20 @@ package com.github.hespercq.fastprojectilefix.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.network.protocol.game.ClientboundSetEntityMotionPacket;
+import net.minecraft.world.phys.Vec3;
 
 @Mixin(ClientboundSetEntityMotionPacket.class)
 public class MixinClientboundSetEntityMotionPacket {
+
+	@Inject(method = "<init>(ILnet/minecraft/world/phys/Vec3;)V", at = @At("HEAD"))
+	private void test(int id, Vec3 deltaMovement, CallbackInfo ci) {
+		System.out.println("MIXIN WORKS");
+	}
 
 	@Redirect(method = "<init>(ILnet/minecraft/world/phys/Vec3;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Mth;clamp(DDD)D"))
 	private double removeClamp(double value, double min, double max) {
